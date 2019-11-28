db0633c2aef2a6ec9c7bcb12d16802b043ae7414
Keerthi Mani <carkeane.getz@gmail.com>
2019-11-28 11:44:27 -0500

    linked to signup and login pages from homepage

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 const Landing = () => {
   return (
     <section className="landing">
@@ -10,12 +12,12 @@ const Landing = () => {
             other friends
           </p>
           <div className="buttons">
-            <a href="register.html" className="btn btn-primary">
+            <Link to="/register" className="btn btn-primary">
               Sign Up
-            </a>
-            <a href="login.html" className="btn btn-light">
+            </Link>
+            <Link to="/login" className="btn btn-light">
               Login
-            </a>
+            </Link>
           </div>
         </div>
       </div>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,22 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div>
       <nav className="navbar bg-dark">
         <h1>
-          <a href="index.html">
+          <Link to="/">
             <i className="fas fa-code"></i> Friends Connector
-          </a>
+          </Link>
         </h1>
         <ul>
           <li>
             <a href="profiles.html">Friends</a>
           </li>
           <li>
-            <a href="register.html">Register</a>
+            <Link to="/register">Register</Link>
           </li>
           <li>
-            <a href="login.html">Login</a>
+            <Link to="/login">Login</Link>
           </li>
         </ul>
       </nav>
