87294b1587ad367901bc8c919baa4be4e900cf3e
Keerthi Mani <carkeane.getz@gmail.com>
2019-11-28 11:18:16 -0500

    homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from "react";
 import "./App.css";
-
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
 const App = () => (
   <Fragment>
-    <h1>App</h1>
+    <Navbar />
+    <Landing />
   </Fragment>
 );
 
