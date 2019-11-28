c77784dd5e8e484c3df9dc67899115dcc4d2477c
Keerthi Mani <carkeane.getz@gmail.com>
2019-11-28 15:41:03 -0500

    handleinput and onsubmit functions are added in the registeration form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,42 +1,85 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: ""
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  //Changes handled When user inputs the value
+  const onChange = e =>
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+
+  //When user submit the form
+  const onSubmit = event => {
+    event.preventDefault();
+    if (password !== password2) {
+      console.log("Password do not match");
+    } else {
+      console.log("SUCCESS");
+    }
+  };
+
   return (
     <Fragment>
-      <h1 class="large text-primary">Sign Up</h1>
-      <p class="lead">
-        <i class="fas fa-user"></i> Create Your Account
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Create Your Account
       </p>
-      <form class="form" action="create-profile.html">
-        <div class="form-group">
-          <input type="text" placeholder="Name" name="name" required />
+      <form className="form" onSubmit={e => onSubmit()}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={e => onChange(e)}
+            required
+          />
         </div>
-        <div class="form-group">
-          <input type="email" placeholder="Email Address" name="email" />
-          <small class="form-text">
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={e => onChange(e)}
+          />
+          <small className="form-text">
             This site uses Gravatar so if you want a profile image, use a
             Gravatar email
           </small>
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <input
             type="password"
             placeholder="Password"
             name="password"
+            value={password}
+            onChange={e => onChange(e)}
             minLength="6"
           />
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <input
             type="password"
             placeholder="Confirm Password"
             name="password2"
+            value={password2}
+            onChange={e => onChange(e)}
             minLength="6"
           />
         </div>
-        <input type="submit" class="btn btn-primary" value="Register" />
+        <input type="submit" className="btn btn-primary" value="Register" />
       </form>
-      <p class="my-1">
+      <p className="my-1">
         Already have an account? <a href="login.html">Sign In</a>
       </p>
     </Fragment>
