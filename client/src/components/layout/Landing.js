938f87aa1623da3670bfcc5299448b888c60db8a
Keerthi Mani <carkeane.getz@gmail.com>
2019-11-28 11:14:19 -0500

    components of Nav and Landing

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,25 @@
+import React from "react";
+const Landing = () => {
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Friends Connector</h1>
+          <p className="lead">
+            Create a Friends profile/portfolio, share posts and get help from
+            other friends
+          </p>
+          <div className="buttons">
+            <a href="register.html" className="btn btn-primary">
+              Sign Up
+            </a>
+            <a href="login.html" className="btn btn-light">
+              Login
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Landing;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar bg-dark">
+        <h1>
+          <a href="index.html">
+            <i className="fas fa-code"></i> Social-Web-App
+          </a>
+        </h1>
+        <ul>
+          <li>
+            <a href="profiles.html">Friends</a>
+          </li>
+          <li>
+            <a href="register.html">Register</a>
+          </li>
+          <li>
+            <a href="login.html">Login</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
