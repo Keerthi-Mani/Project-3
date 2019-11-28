5b5b2aca5bbf6c9b5b57b4e8402d6f40666c8c28
Keerthi Mani <carkeane.getz@gmail.com>
2019-11-28 17:32:18 -0500

    settimeout

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -9,4 +9,7 @@ export const setAlert = (msg, alertType) => dispatch => {
     type: SET_ALERT,
     payload: { msg, alertType, id }
   });
+
+  //set time to remove alert
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), 5000);
 };
