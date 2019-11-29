c571d0106db5d22a23742213c6975fb818cb2e90
Keerthi Mani <carkeane.getz@gmail.com>
2019-11-29 12:12:50 -0500

    package.json connected the frontend and backend

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -5,7 +5,7 @@ import { connect } from "react-redux";
 
 const PrivateRoute = ({
   component: Component,
-  auth: { isAuthenticated },
+  auth: { isAuthenticated, loading },
   ...rest
 }) => (
   <Route
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const connectDB = require('./config/db');
+const express = require("express");
+const mongoose = require("mongoose");
+const connectDB = require("./config/db");
 
 const app = express();
 //Connect Database
@@ -8,17 +8,16 @@ const app = express();
 connectDB();
 
 //Init Middleware
-app.use(express.json({extended : false}));
+app.use(express.json({ extended: false }));
 
-
-app.get('/', (req,res) => res.send('API Running'));
+app.get("/", (req, res) => res.send("API Running"));
 
 //Define Routes
-app.use('/api/users',require('./routes/api/users'));
-app.use('/api/auth',require('./routes/api/auth'));
-app.use('/api/profile',require('./routes/api/profile'));
-app.use('/api/posts',require('./routes/api/posts'))
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
