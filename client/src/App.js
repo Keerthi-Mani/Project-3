e1725fa0d4b3cc999edbb3e1a3bcff14e442e49b
Keerthi Mani <carkeane.getz@gmail.com>
2019-11-29 10:12:06 -0500

    dashboard folder and file in component

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
+import Dashboard from "./components/dashboard/Dashboard";
+
 //redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -34,6 +36,7 @@ const App = () => {
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
