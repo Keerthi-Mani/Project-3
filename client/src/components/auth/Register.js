f1c0d28cd6fd333d238f4cf453b5a6d4c0a1fc61
Keerthi Mani <carkeane.getz@gmail.com>
2019-11-29 05:05:44 -0500

    Registeration is  success or fail

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,9 +2,10 @@ import React, { Fragment, useState } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { setAlert } from "../../actions/alert";
+import { register } from "../../actions/auth";
 import PropTypes from "prop-types";
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -27,7 +28,8 @@ const Register = ({ setAlert }) => {
     if (password !== password2) {
       setAlert("Password do not match", "danger");
     } else {
-      console.log("SUCCESS");
+      //console.log("SUCCESS");
+      register({ name, email, password });
     }
   };
 
@@ -91,6 +93,7 @@ const Register = ({ setAlert }) => {
 };
 
 Register.prototype = {
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired
 };
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
