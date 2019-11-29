aeb7539d7676b8c2fcda7002e9c728a3007afbc1
Shital Patel <shital732@yahoo.com>
2019-11-29 16:34:30 -0500

    Change db storage to atlas and Profile models and route

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const connectDB = require('./config/db');
+const express = require("express");
+const mongoose = require("mongoose");
+const connectDB = require("./config/db");
 
 const app = express();
-//Connect Database
 
+//Connect Database
 connectDB();
 
 //Init Middleware
-app.use(express.json({extended : false}));
+app.use(express.json({ extended: false }));
 
-
-app.get('/', (req,res) => res.send('API Running'));
+app.get("/", (req, res) => res.send("API Running"));
 
 //Define Routes
-app.use('/api/users',require('./routes/api/users'));
-app.use('/api/auth',require('./routes/api/auth'));
-app.use('/api/profile',require('./routes/api/profile'));
-app.use('/api/posts',require('./routes/api/posts'))
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
