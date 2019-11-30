40a4491ec3cf987274127249d12a85e6baba7218
Keerthi Mani <carkeane.getz@gmail.com>
2019-11-30 12:05:09 -0500

    create profile form

--- models/Profile.js
+++ models/Profile.js
@@ -5,6 +5,12 @@ const ProfileSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "user"
   },
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
   location: {
     type: String
   },
@@ -18,6 +24,79 @@ const ProfileSchema = new mongoose.Schema({
   },
   bio: {
     type: String
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      company: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofstudy: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
   },
   date: {
     type: Date,
