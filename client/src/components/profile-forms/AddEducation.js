7c25fd25e8d24236b34bc5cf5f6e425fcec4e67c
Keerthi Mani <carkeane.getz@gmail.com>
2019-12-01 08:50:40 -0500

    profile component

--- client/src/components/profile-forms/AddEducation.js
+++ client/src/components/profile-forms/AddEducation.js
@@ -132,4 +132,4 @@ addEducation.propTypes = {
   addEducation: PropTypes.func.isRequired
 };
 
-export default connect(null, { addEducation })(AddEducation);
+export default connect(null, { addEducation })(withRouter(AddEducation));
