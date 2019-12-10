2a8b19dda3e6f10e118fdfe22b935ea49a16bd4a
Keerthi Mani <carkeane.getz@gmail.com>
2019-12-10 16:14:50 -0500

    build is installed

---
+++ client/build/precache-manifest.fe65c878bc268fea07057e548dd0621a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e867849bd9027874c4d8cd19f07838",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2f9757a3819826f8870",
+    "url": "/static/css/2.31352408.chunk.css"
+  },
+  {
+    "revision": "63a0ceca23fd6386e8e4",
+    "url": "/static/css/main.2b420e87.chunk.css"
+  },
+  {
+    "revision": "a2f9757a3819826f8870",
+    "url": "/static/js/2.a35683f4.chunk.js"
+  },
+  {
+    "revision": "7d9c9eaa9d599373e2f04e5e88f6996b",
+    "url": "/static/js/2.a35683f4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "63a0ceca23fd6386e8e4",
+    "url": "/static/js/main.f04c9142.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  },
+  {
+    "revision": "6a57bfa487f526d4545c15f98b10c786",
+    "url": "/static/media/background.6a57bfa4.gif"
+  },
+  {
+    "revision": "211c121e8ed4abcdcdcfb63f4bbbfffd",
+    "url": "/static/media/sky.211c121e.jpg"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/static/media/spinner.26ef8bb4.gif"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
